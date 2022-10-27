38052b77813e21cec03ec7af64f7be95f14b4502
HIDRI A <agthidri24@yahoo.com>
2022-10-27 10:14:12 -0500

    this commit will add the first commit

---
+++ src/main/java/com/example/ramazone/RamazoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamazoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamazoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ramazone/RamazoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramazone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamazoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
